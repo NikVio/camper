1a4ddf96ad6908e2c445e93d8662d236e7868a1c
NikVio <131026216+NikVio@users.noreply.github.com>
2024-03-23 17:20:19 +0200

    add gh-pages

--- my-app/src/components/CatalogCampers/CatalogCampers.jsx
+++ my-app/src/components/CatalogCampers/CatalogCampers.jsx
@@ -1,19 +1,17 @@
 export const CatalogCampers = ({
-  item: { gallery, name, price, rating, location, description },
+  item: { photo, name, price, rating, location, description },
 }) => {
   return (
     <div>
       <div>
-        <img src={gallery} alt={name} />
+        <img src={photo} alt={name} />
       </div>
       <div>
-        <li>
-          <h3>{name}</h3>
-          <p>{price}</p>
-          <p>{rating}</p>
-          <p>{location}</p>
-          <p>{description}</p>
-        </li>
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <p>{rating}</p>
+        <p>{location}</p>
+        <p>{description}</p>
       </div>
     </div>
   );
